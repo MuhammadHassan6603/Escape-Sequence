04b442131b81221e58816509d8b54f18bedbbb80
Muhammad Hassan <105521572+MuhammadHassan6603@users.noreply.github.com>
2022-05-19 21:54:31 +0500

    Add files via upload

---
+++ Escape.java
@@ -0,0 +1,13 @@
+public class Escape
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hi\nHello");
+		System.out.println("One\rTwo");
+		System.out.println("A\bB");
+		System.out.println("Hassan\tIs\tInnocent");
+		System.out.println("\"Hassan\"");
+		System.out.println("\'Hehe\'");
+		System.out.println("\\NFC\\");
+	}
+}
